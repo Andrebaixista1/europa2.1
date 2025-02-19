e6a626a9521ebd9b0911f4f98f423148cec35e40
André Felipe Pinto <andrbter@gmail.com>
2025-02-19 08:41:06 -0300

    aviso in100 fora

--- src/components/ChangelogOverlay.js
+++ src/components/ChangelogOverlay.js
@@ -39,10 +39,11 @@ const ChangelogOverlay = ({ onClose }) => {
         <div>IP bloqueado ? Acesse (<a href="https://meuip.com.br" target="_blank">https://meuip.com.br</a>) e envie para o <b>Gerente Expande</b> ou <b>Planejamento </b>.</div>
         <br/>
         <ul>
-          <li>17/02 - Adicionada verificação de status da API.</li>
+          {/* <li>17/02 - Adicionada verificação de status da API.</li> */}
           <li>17/02 - Quantidade de total no final das colunas.</li>
           <li>17/02 - Agora teremos restrição de IP para sua segurança, se perceber<br/> que o arquivo não esta sendo higienizado entre em contato com o seu <b>Gerente Expande</b> ou com <b>Planejamento.</b> <br/></li>
           <li>18/02 - Agora temos uma nova notificação, caso seu IP esteja bloqueado você será avisado! Não se preocupe, mande seu IP Externo para o seu <b>Gerente Expande</b> e/ou <b>Planejamento</b> para que seja liberado</li>
+          <li style={{ color: 'red', fontWeight: 'bold' }}>19/02 - Devido a uma instabilidade o sistema de in100 está fora!</li>
         </ul>
         <br/>
         {/* <div><b>Obs.:</b> <i>Envie o IP se nenhum nome estiver sendo higienizado !</i></div> */}
