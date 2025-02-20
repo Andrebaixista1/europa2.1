642d4a920f364e56bc8c8ae7fd801017262cbb99
André Felipe Pinto <andrbter@gmail.com>
2025-02-20 11:24:42 -0300

    is new black - copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer style={{ textAlign: "center", marginTop: "40px" }}>
-      &copy; Todos os direitos reservados a Vieiracred 2025 | Criado e Desenvolvido por André Felipe | INB (Is New Black) v1.1.202502
+      &copy; Todos os direitos reservados a Vieiracred 2025 | Criado e Desenvolvido por André Felipe | INB (Is New Black)
     </footer>
   );
 }
